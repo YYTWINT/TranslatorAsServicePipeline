ebb9d7de43bf711493e247aa03163ad15e483f41
YYTWINT <yytwint.sisw@siemens.com>
2022-02-21 18:16:51 +0530

    Update TranslatorWorkerScripts.groovy

--- TranslatorWorkerScripts.groovy
+++ TranslatorWorkerScripts.groovy
@@ -2,7 +2,9 @@ def createUnit()
 {
 	echo "Creating unit..."
 	script{		
-		
+		def unitFullPath="${params.UnitPath}/${params.NXRelease}_TranslatorWorker_${BUILD_TIMESTAMP}"
+		sh "chmod +x ./createTranslatorWorkerUnit.sh "
+		sh "./createTranslatorWorkerUnit.sh ${params.NXRelease} ${unitFullPath}"		
 	}
 }
 
@@ -10,7 +12,9 @@ def buildUnit()
 {
 	echo "Building unit..."
 	script{		
-		
+		def unitFullPath="${params.UnitPath}/${params.NXRelease}_TranslatorWorker_${BUILD_TIMESTAMP}"
+		sh "chmod +x ./buildTranslatorWorkerUnit.sh "
+		sh "./buildTranslatorWorkerUnit.sh ${unitFullPath}"		
 	}
 }
 
@@ -18,15 +22,23 @@ def TestUnit()
 {
 	echo "Executing devtests..."
 	script{		
-			
+		def unitFullPath="${params.UnitPath}/${params.NXRelease}_TranslatorWorker_${BUILD_TIMESTAMP}"
+		sh "chmod +x ./executeTranslatorWorkerTest.sh "
+		sh "./executeTranslatorWorkerTest.sh ${unitFullPath}"		
 	}
 }
 
 def StageAndDeploy()
 {
-	echo "Executing stage and deploy ..."
+	echo "Executing stage and deploy..."
 	script{		
-			
+		def unitFullPath="${params.UnitPath}/${params.NXRelease}_TranslatorWorker_${BUILD_TIMESTAMP}"
+		def stagePath="${params.StagePath}/${params.NXRelease}_TranslatorWorker_${BUILD_TIMESTAMP}"
+		def customerId="${params.Customer}"
+		def deployFlag="${params.Deploy}"
+		
+		sh "chmod +x ./stageAndTranslatorWorkerUnit.sh "
+		sh "./stageAndTranslatorWorkerUnit.sh ${unitFullPath} ${stagePath} 'Artifacts/${customerId}' ${deployFlag}"		
 	}
 }
 
