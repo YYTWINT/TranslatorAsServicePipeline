bfce0d1e1ff3c04b9fd108a54edaf15dc47598b0
YYTWINT <yytwint.sisw@siemens.com>
2022-02-22 12:36:03 +0530

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -2,21 +2,19 @@ def sendSuccessEmail(String buildDir, String stageDir)
 {
 	def subject = "Job Executed '${env.JOB_NAME} - [${env.BUILD_NUMBER}] - ${currentBuild.currentResult}'"
 	def details = """
-				 <p>Job - '${env.JOB_NAME}' </p>
-				 <p>Build number - '[${env.BUILD_NUMBER}]'</p>
-				 <p>NX Release - '${params.NXRelease}'</p>
-				 <p>Unit path - '${buildDir}'</p>
-				 <p>Stage path - '${stageDir}'</p>
-				 <p>Customer - '${params.Customer}'</p>
-				 <p>Deploy flag - '${params.Deploy}'</p>
-				 <p>Status - '${currentBuild.currentResult}' </p>				 
-				 <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
-
+				<tr>
+					<td>
+						Job
+					</td>
+					<td>
+						'${env.JOB_NAME}' 
+					</td>
+				</tr>				
 				 """
 	emailext(
 		subject: subject,
 		body: details,
-		to: 'yytwint.sisw@siemens.com,nilesh.lakhotia@siemens.com,dattaprasad.sonawadekar@siemens.com,rakesh.thakur@siemens.com'
+		to: 'rakesh.thakur@siemens.com'
 	)
 }
 return this
