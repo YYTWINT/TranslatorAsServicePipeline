8c06032103a6f54f6bb484c2f37f64f564b5ed2c
YYTWINT <yytwint.sisw@siemens.com>
2022-02-22 12:43:15 +0530

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -2,24 +2,33 @@ def sendSuccessEmail(String buildDir, String stageDir)
 {
 	def subject = "Job Executed '${env.JOB_NAME} - [${env.BUILD_NUMBER}] - ${currentBuild.currentResult}'"
 	def details = """
-				<table>
-					<thead>
-					  <tr>
-						<th>ABC</th>
-						<th>PQR</th>
-					  </tr>
-					</thead>
-					<tbody>
-					  <tr>
-						<td>Job</td>
-						<td>${env.JOB_NAME}</td>
-					  </tr>
-					  <tr>
-						<td>2</td>
-						<td>b</td>
-					  </tr>
-					</tbody>
-				</table>		
+				<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+.tg .tg-ur59{border-color:#343434;text-align:left;vertical-align:top}
+</style>
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-0lax">ABC</th>
+    <th class="tg-0lax">PQR</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-0lax">1</td>
+    <td class="tg-0lax">a</td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">2</td>
+    <td class="tg-ur59">b</td>
+  </tr>
+</tbody>
+</table>	
 				 """
 	emailext(
 		subject: subject,
