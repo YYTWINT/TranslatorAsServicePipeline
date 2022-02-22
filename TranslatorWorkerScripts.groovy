426add5ffceb7ce01bd4225dd5e1f7b67ed3e972
YYTWINT <yytwint.sisw@siemens.com>
2022-02-22 15:27:00 +0530

    Update TranslatorWorkerScripts.groovy

--- TranslatorWorkerScripts.groovy
+++ TranslatorWorkerScripts.groovy
@@ -1,31 +1,55 @@
 def createUnit(String buildDir)
 {
 	echo "Creating unit..."
-	
+	script{		
+		def unitFullPath="${buildDir}"
+		sh "chmod +x ./createTranslatorWorkerUnit.sh "
+		sh "./createTranslatorWorkerUnit.sh ${params.NXRelease} ${unitFullPath}"		
+	}
 }
 
 def buildUnit(String buildDir)
 {
 	echo "Building unit..."
-	
+	script{		
+		def unitFullPath="${buildDir}"
+		sh "chmod +x ./buildTranslatorWorkerUnit.sh "
+		sh "./buildTranslatorWorkerUnit.sh ${unitFullPath}"		
+	}
 }
 
 def TestUnit(String buildDir)
 {
 	echo "Executing devtests..."
-	
+	script{		
+		def unitFullPath="${buildDir}"
+		sh "chmod +x ./executeTranslatorWorkerTest.sh "
+		sh "./executeTranslatorWorkerTest.sh ${unitFullPath}"		
+	}
 }
 
 def StageAndDeploy(String buildDir, String stageDir)
 {
 	echo "Executing stage and deploy..."
-	
+	script{		
+		def unitFullPath="${buildDir}"
+		def stagePath="${stageDir}"
+		def customerId="${params.Customer}"
+		def deployFlag="${params.Deploy}"
+		
+		sh "chmod +x ./stageAndDeployTranslatorWorkerUnit.sh "
+		sh "./stageAndDeployTranslatorWorkerUnit.sh ${unitFullPath} ${stagePath} 'Artifacts/${customerId}' ${deployFlag}"		
+	}
 }
 
 def Purge(String dirName)
 {
-	echo "Executing Purge ${dirName} ..."
-	
+	echo "Executing Purge ..."
+	script{		
+		def fullPath="${dirName}"
+		sh "chmod +x ./purge.sh "
+		sh "./purge.sh ${fullPath}"		
+	}
 }
 
 return this
