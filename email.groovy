6ccac51b352532c440d91c3e31b4bf2442cdab5b
YYTWINT <yytwint.sisw@siemens.com>
2022-02-22 12:41:15 +0530

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -2,14 +2,24 @@ def sendSuccessEmail(String buildDir, String stageDir)
 {
 	def subject = "Job Executed '${env.JOB_NAME} - [${env.BUILD_NUMBER}] - ${currentBuild.currentResult}'"
 	def details = """
-				<tr>
-					<td>
-						Job
-					</td>
-					<td>
-						'${env.JOB_NAME}' 
-					</td>
-				</tr>				
+				<table>
+					<thead>
+					  <tr>
+						<th>ABC</th>
+						<th>PQR</th>
+					  </tr>
+					</thead>
+					<tbody>
+					  <tr>
+						<td>Job</td>
+						<td>${env.JOB_NAME}</td>
+					  </tr>
+					  <tr>
+						<td>2</td>
+						<td>b</td>
+					  </tr>
+					</tbody>
+				</table>		
 				 """
 	emailext(
 		subject: subject,
