b7759c039ed04221a8523b79a5d3cecbe605b04e
YYTWINT <yytwint.sisw@siemens.com>
2022-04-22 15:43:19 +0530

    To support skipping fail(fail) cases scenario

--- executeTranslatorWorkerTest.sh
+++ executeTranslatorWorkerTest.sh
@@ -10,11 +10,17 @@ fi
 
 UNIT_PATH=$1
 export SPLM_LICENSE_SERVER=28000@pnlv6002,28000@inpnsweb01 
-/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
+/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6 -parent
 casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
 if [ $casesFailed != 0 ]
 then
 	/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runfails -p 6 -keep -local
+	casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
+	if [ $casesFailed != 0 ]
+	then
+		echo "Number of auto test cases failed = $casesFailed...Exiting with error."
+		exit 1
+	fi
 else
 	echo "No test case failed. So skipping re-run of devtest."
 fi
