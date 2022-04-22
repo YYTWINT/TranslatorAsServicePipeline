af9be79bb48517850fd1a7060957cfb379ec9583
YYTWINT <yytwint.sisw@siemens.com>
2022-04-22 15:59:31 +0530

    Update executeTranslatorWorkerTest.sh

--- executeTranslatorWorkerTest.sh
+++ executeTranslatorWorkerTest.sh
@@ -10,7 +10,7 @@ fi
 
 UNIT_PATH=$1
 export SPLM_LICENSE_SERVER=28000@pnlv6002,28000@inpnsweb01 
-/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6 -parent
+/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
 casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
 if [ $casesFailed != 0 ]
 then
