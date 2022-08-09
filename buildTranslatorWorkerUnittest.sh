2d307df6c76ca693eec40027601f37962bfce9b4
YYTWINT <99640734+YYTWINT@users.noreply.github.com>
2022-08-09 21:36:29 +0530

    Update buildTranslatorWorkerUnittest.sh

--- buildTranslatorWorkerUnittest.sh
+++ buildTranslatorWorkerUnittest.sh
@@ -13,9 +13,11 @@ CP_NUMBER=$2
 HC_FLAG=$3
 if [ ${HC_FLAG} == "false" ]
 then
+	echo "inside build with false HC"
 	#/usr/site/devop_tools/bin/unit run ${UNIT_PATH} b product TranslatorWorker
 	#/usr/site/devop_tools/bin/unit run ${UNIT_PATH} b product validate_worker TranslatorWorker
 else
+	echo "inside build with true HC"
 	# /usr/site/devop_tools/bin/unit run ${UNIT_PATH} /usr/site/devop_tools/bin/dt cli set -C -A ${CP_NUMBER}
 	# /usr/site/devop_tools/bin/unit run ${UNIT_PATH} b 
 	# /usr/site/devop_tools/bin/unit run ${UNIT_PATH} b image ugtopv
