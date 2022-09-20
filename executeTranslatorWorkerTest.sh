7a1a1243e62215587488d9565c8666e9339889d6
YYTWINT <yytwint.sisw@siemens.com>
2022-09-20 17:43:21 +0530

    changing license details

--- executeTranslatorWorkerTest.sh
+++ executeTranslatorWorkerTest.sh
@@ -9,7 +9,7 @@ then
 fi
 
 UNIT_PATH=$1
-export SPLM_LICENSE_SERVER=28000@pnlv6002,28000@inpnsweb01 
+export SPLM_LICENSE_SERVER=29000@pnnxflex3:29000@pnnxflex4 
 /usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
 casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
 if [ $casesFailed != 0 ]
